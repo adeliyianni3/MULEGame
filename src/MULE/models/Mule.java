543ddf7a318cac787c1b1992d420fa60d0a4b4c7
adeliyianni3 <adeliyianni5@gatech.edu>
2015-09-17 18:46:33 -0400

    Mule class

---
+++ src/MULE/models/Mule.java
@@ -0,0 +1,12 @@
+package MULE.models;
+
+/**
+ * Created by Antonia on 9/17/2015.
+ */
+public class Mule {
+    //Started Mule class
+    private String resourceIs;
+    public Mule() {
+        this.resourceIs = null;
+    }
+}
