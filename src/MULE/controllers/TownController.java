caf15af89b3a48f668a1eb1891e722e5519c2254
ETVargas <evargas30@gatech.edu>
2015-09-18 22:12:31 -0400

    Resized the map to hold 9x5 tiles. Tweaked MouseEvent in TownController.

--- src/MULE/controllers/TownController.java
+++ src/MULE/controllers/TownController.java
@@ -3,6 +3,7 @@ package MULE.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 
 /**
  * Created by Ethan on 9/18/2015.
@@ -10,7 +11,7 @@ import javafx.scene.Node;
 public class TownController {
 
     @FXML
-    void storeClick(ActionEvent event) {
+    void storeClick(MouseEvent event) {
         String storeName = ((Node)event.getSource()).getId();
         //add in interaction with main here
         //landName will contain a string "RC" containing store's row and column in the set 2x2 array
