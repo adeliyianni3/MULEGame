a138ae4b00f4fca2eea2a7ddc179dc81715e9ba0
adeliyianni3 <adeliyianni5@gatech.edu>
2015-10-27 20:49:31 -0400

    noa-rgs

--- src/MULE/models/EventThree.java
+++ src/MULE/models/EventThree.java
@@ -2,11 +2,14 @@ package MULE.models;
 
 import MULE.controllers.Game;
 
+import java.awt.*;
+
 /**
  * Created by Antonia on 10/23/2015.
  */
 public class EventThree implements RandomEvent {
     private static int[] roundsM = {25, 25, 25, 50, 50, 50, 50, 75, 75, 75, 75, 100};
+    public EventThree(){}
     @Override
     public String apply(Player p) {
             int m = roundsM[Game.instance.getRound() - 3];
