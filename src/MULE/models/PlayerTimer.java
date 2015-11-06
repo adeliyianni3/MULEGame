d9cd310734b4ac04771874c126509d270f6bd3e0
ETVargas <evargas30@gatech.edu>
2015-11-06 10:41:34 -0500

    Javadoc for PlayerTimer.

--- src/MULE/models/PlayerTimer.java
+++ src/MULE/models/PlayerTimer.java
@@ -11,14 +11,37 @@ import javafx.application.Platform;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
+/**
+ * Helper class wrapping a timer to keep track of game flow.
+ */
 public class PlayerTimer {
-    public PlayerTimer(){}
+    /**
+     * No-args constructor.
+     */
+    public PlayerTimer() {
+    }
+
+    /**
+     * Seconds remaining in timer.
+     */
     private int secs;
+    /**
+     * Java Timer performing the back-end stuff.
+     */
     private static Timer timer;
+    /**
+     * Array indicating the amount of food needed to survive each round.
+     */
     private final int[] foodNeeded = {3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5};
 
-    public int computeTime(Player p, int round){
+    /**
+     * Determines the amount of time remaining for
+     *  the player based on their food.
+     * @param p Player who is taking their turn
+     * @param round Current round number
+     * @return Time the Player has to take their turn
+     */
+    public final int computeTime(final Player p, final int round) {
         int food = p.foodCounter();
         if (food > 0 && food < foodNeeded[round - 3]) {
             return 30;
@@ -29,7 +52,10 @@ public class PlayerTimer {
         }
     }
 
-    public void startTime() {
+    /**
+     * Starts the timer over for the next player.
+     */
+    public final void startTime() {
         secs = computeTime(Game.instance.players[Game.instance.getTurn() - 1],
                 Game.instance.getRound());
         int delay = 1000;
@@ -44,7 +70,11 @@ public class PlayerTimer {
             }
         }, delay, period);
     }
-    public void stopTime() {
+
+    /**
+     * Stops the timer.
+     */
+    public final void stopTime() {
         if (timer == null) {
             timer = new Timer();
         }
@@ -59,9 +89,18 @@ public class PlayerTimer {
             ScreenNavigator.instance.loadEndGame();
         }
     }
-    public int getTime() {
+
+    /**
+     * Gets the amount of time remaining in the timer.
+     * @return Amount of time remaining in the timer
+     */
+    public final int getTime() {
         return secs;
     }
+
+    /**
+     * Sets the interval of the timer.
+     */
     private void setInterval() {
         if (secs == 1) {
             if (Game.instance.getCurrentState() != Game.State.MAP) {
